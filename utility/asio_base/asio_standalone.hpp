8d14733a5b0f95b267cfd523a80a6cd6e3a623b3
yandaren <yandaren1220@126.com>
2018-05-21 09:56:59 +0800

    a domain name resolution and ip class utility base on asio

---
+++ utility/asio_base/asio_standalone.hpp
@@ -0,0 +1,16 @@
+﻿/**
+ *
+ * config asio as standalone(no boost dependency)
+ *
+ * @author  :   yandaren1220@126.com
+ * @date    :   2017-08-25
+ */
+
+#ifndef __ydk_utility_asio_base_asio_standalone_config_hpp__
+#define __ydk_utility_asio_base_asio_standalone_config_hpp__
+
+#ifndef ASIO_STANDALONE
+#define ASIO_STANDALONE
+#endif
+
+#endif
---
+++ utility/asio_base/deadline_timer_service.hpp
@@ -0,0 +1,168 @@
+﻿//
+// deadline_timer_service.hpp
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2003-2015 Christopher M. Kohlhoff (chris at kohlhoff dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef __ydk_utility_asio_base_dealine_timer_service_hpp__
+#define __ydk_utility_asio_base_dealine_timer_service_hpp__
+
+#if defined(_MSC_VER) && (_MSC_VER >= 1200)
+# pragma once
+#endif // defined(_MSC_VER) && (_MSC_VER >= 1200)
+
+#include "asio/detail/config.hpp"
+#include <cstddef>
+#include <chrono>
+#include "asio/async_result.hpp"
+#include "asio/io_service.hpp"
+#include "asio/wait_traits.hpp"
+#include "asio/detail/deadline_timer_service.hpp"
+#include "asio/detail/timer_queue_ptime.hpp"
+#include "asio/detail/chrono_time_traits.hpp"
+#include "asio/detail/push_options.hpp"
+
+namespace utility{
+
+namespace asio_base {
+
+/// Default service implementation for a timer.
+template <typename TimeType,
+    typename TimeTraits = asio::detail::chrono_time_traits<TimeType, asio::wait_traits<TimeType>> >
+class deadline_timer_service
+#if defined(GENERATING_DOCUMENTATION)
+  : public asio::io_service::service
+#else
+  : public asio::detail::service_base<
+      deadline_timer_service<TimeType, TimeTraits> >
+#endif
+{
+public:
+#if defined(GENERATING_DOCUMENTATION)
+  /// The unique service identifier.
+  static asio::io_service::id id;
+#endif
+
+  /// The time traits type.
+  typedef TimeTraits traits_type;
+
+  /// The time type.
+  typedef typename traits_type::time_type time_type;
+
+  /// The duration type.
+  typedef typename traits_type::duration_type duration_type;
+
+private:
+  // The type of the platform-specific implementation.
+  typedef asio::detail::deadline_timer_service<traits_type> service_impl_type;
+
+public:
+  /// The implementation type of the deadline timer.
+#if defined(GENERATING_DOCUMENTATION)
+  typedef implementation_defined implementation_type;
+#else
+  typedef typename service_impl_type::implementation_type implementation_type;
+#endif
+
+  /// Construct a new timer service for the specified io_service.
+  explicit deadline_timer_service(asio::io_service& io_service)
+    : asio::detail::service_base<
+        deadline_timer_service<TimeType, TimeTraits> >(io_service),
+      service_impl_(io_service)
+  {
+  }
+
+  /// Construct a new timer implementation.
+  void construct(implementation_type& impl)
+  {
+    service_impl_.construct(impl);
+  }
+
+  /// Destroy a timer implementation.
+  void destroy(implementation_type& impl)
+  {
+    service_impl_.destroy(impl);
+  }
+
+  /// Cancel any asynchronous wait operations associated with the timer.
+  std::size_t cancel(implementation_type& impl, asio::error_code& ec)
+  {
+    return service_impl_.cancel(impl, ec);
+  }
+
+  /// Cancels one asynchronous wait operation associated with the timer.
+  std::size_t cancel_one(implementation_type& impl,
+      asio::error_code& ec)
+  {
+    return service_impl_.cancel_one(impl, ec);
+  }
+
+  /// Get the expiry time for the timer as an absolute time.
+  time_type expires_at(const implementation_type& impl) const
+  {
+    return service_impl_.expiry(impl);
+  }
+
+  /// Set the expiry time for the timer as an absolute time.
+  std::size_t expires_at(implementation_type& impl,
+      const time_type& expiry_time, asio::error_code& ec)
+  {
+    return service_impl_.expires_at(impl, expiry_time, ec);
+  }
+
+  /// Get the expiry time for the timer relative to now.
+  duration_type expires_from_now(const implementation_type& impl) const
+  {
+    return TimeTraits::subtract(service_impl_.expiry(impl), TimeTraits::now());
+  }
+
+  /// Set the expiry time for the timer relative to now.
+  std::size_t expires_from_now(implementation_type& impl,
+      const duration_type& expiry_time, asio::error_code& ec)
+  {
+    return service_impl_.expires_after(impl, expiry_time, ec);
+  }
+
+  // Perform a blocking wait on the timer.
+  void wait(implementation_type& impl, asio::error_code& ec)
+  {
+    service_impl_.wait(impl, ec);
+  }
+
+  // Start an asynchronous wait on the timer.
+  template <typename WaitHandler>
+  ASIO_INITFN_RESULT_TYPE(WaitHandler,
+      void (asio::error_code))
+  async_wait(implementation_type& impl,
+      ASIO_MOVE_ARG(WaitHandler) handler)
+  {
+    asio::async_completion<WaitHandler,
+      void (asio::error_code)> init(handler);
+
+    service_impl_.async_wait(impl, init.handler);
+
+    return init.result.get();
+  }
+
+private:
+  // Destroy all user-defined handler objects owned by the service.
+  void shutdown_service()
+  {
+    service_impl_.shutdown_service();
+  }
+
+  // The platform-specific implementation.
+  service_impl_type service_impl_;
+};
+
+} // namespace asio_base
+
+} // namespace utility
+
+#include "asio/detail/pop_options.hpp"
+
+#endif // ASIO_DEADLINE_TIMER_SERVICE_HPP
